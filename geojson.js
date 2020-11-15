672538eff9437a6a7397fc2177077edafe1f19ee
Kārlis Kurgs <karlis.kurgs@gmail.com>
2020-11-15 16:03:06 +0200

    fucking around

---
+++ geojson.js
@@ -0,0 +1,22 @@
+var landData = {"type":"FeatureCollection","features":[
+{"type":"Feature","id":"01","properties":{"name":"Lidlauka 7","density":94.65},"geometry":{"type":"Polygon","coordinates":[[[56.414186, 24.229574],
+        [56.411907, 24.212322],
+        [56.404214, 24.221077],
+        [56.406114, 24.238672],
+        [56.413379, 24.234467]]]}}]};
+
+        var gasLine = {
+                "type": "Feature",
+                        "geometry": {
+                            "type": "LineString",
+                            "coordinates": [
+                                [56.415071, 24.212794],
+                                [56.405627, 24.217129],
+                                [56.405277, 24.219650],
+                                [56.403472, 24.224349]
+                            ]
+                        },
+                        "properties": {
+                            "popupContent": "This is a medium pressure gas pipeline",
+                        }
+                    };
--- land.js
+++
@@ -1,6 +0,0 @@
-var landData = {"type":"FeatureCollection","features":[
-{"type":"Feature","id":"01","properties":{"name":"Lidlauka 7","density":94.65},"geometry":{"type":"Polygon","coordinates":[[[56.414186, 24.229574],
-        [56.411907, 24.212322],
-        [56.404214, 24.221077],
-        [56.406114, 24.238672],
-        [56.413379, 24.234467]]]}}]};
